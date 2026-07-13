bbd21b02bbfbecfa94635be6d9a6c8471eb5f2ec
Noura253 <nouralamzara@gmail.com>
2026-07-14 00:01:50 +0100

    flat

---
+++ flat.js
@@ -0,0 +1,14 @@
+function flat(arr){
+let result=[];
+for(let i=0;i<arr.length;i++){
+if(Array.isArray(arr[i])){
+    for(let j=0;j<arr[i].length;j++){
+        result.push(arr[i][j]);
+    }
+}
+else{
+    result.push(arr[i]);
+}
+}
+return result;
+}
